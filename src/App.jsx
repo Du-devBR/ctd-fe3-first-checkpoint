755b609ade0cf4b055087450d9dc1e9d41d39219
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-11-07 22:48:16 -0300

    criado funcionalidade do componente card que é adionado atravez de um array map

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,68 @@
-
+import "./app-style.sass"
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
+import { useState } from "react"
+import { Card } from "./Card"
+
 function App() {
-  // Aqui você irá criar os Estados para manipular os Inputs
+
+  const [nameColor, setNameColor] = useState()
+  const [hexadecimalColor, setHexadecimalColor] = useState()
+  const [allCardColors, setAllCardColors] = useState([])
+
+  function registerNewCard(event){
+    event.preventDefault()
+
+    const newCardColor = {
+      name: nameColor,
+      hexadecimal: hexadecimalColor
+    }
+
+    setAllCardColors([...allCardColors, newCardColor])
+  }
 
 
   return (
     <div className="App">
-     <h1>Carga de estudiantes</h1>
-     <form>
-      {/* Comece a desenvolver o seu Código por aqui :) */}
+     <form onSubmit={event => registerNewCard(event)}>
+      <div className="container-form">
+        <h1>Adicionar nova cor</h1>
+        <div className="input-content">
+          <div className="inout-name-color">
+            <label htmlFor="">Nome</label>
+            <input type="text"
+              placeholder="Nome da cor"
+              onChange={event => setNameColor(event.target.value)}
+            />
+          </div>
+          <div className="input-hexadecimal-color">
+            <label htmlFor="">Cor</label>
+            <input type="text"
+              placeholder="Insira sua cor no formato Hexadecimal"
+              onChange={event => setHexadecimalColor("#" + event.target.value)}
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          >
+          Adicionar
+        </button>
+      </div>
      </form>
+     <section className="container-all-cards">
+      {
+        allCardColors.map(
+          component => {
+            return (
+              <Card
+                data={component}
+              />
+            )
+          }
+        )
+      }
+     </section>
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,12 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
-
-export function Card () {
+import "./card_style.sass"
+export function Card (props) {
   return (
-    <div>
-
+    <div className="container-card" style={{backgroundColor: `${props.data.hexadecimal}`}}>
+      <div className="color-card">
+        <h2 className="name-color">{props.data.name}</h2>
+        <span className="hexadecimal-color">{props.data.hexadecimal}</span>
+      </div>
     </div>
   )
 }
